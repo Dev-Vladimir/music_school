180e924dec98bff8b4898edc6166eab533ddce18
Dev-Vladimir <63652808+Dev-Vladimir@users.noreply.github.com>
2021-11-17 00:51:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+class Menu{
+	constructor(burger, menu){
+		this.menu = menu.closest('.menu');
+		this.burger = burger;
+		let nav = menu.querySelector('ul');
+		this.nav = nav;
+		nav.remove();
+		// console.log(this);
+		this.createChildrenSigns();
+		menu.append(this.nav);
+		this.burger.addEventListener('click', () => this.toggleMenu());
+	}
+	createChildrenSigns(){
+		let childrenUl = this.nav.querySelectorAll('li>ul');
+		console.log(childrenUl);
+		childrenUl.forEach(ul => (ul.closest('li').querySelector('a').insertAdjacentHTML("afterbegin", '<span><</span>')))
+	}
+	toggleMenu(){
+		this.menu.classList.toggle('active');
+		this.burger.classList.toggle('active');
+	}
+}
+
+
+window.onload = function(){
+	new Menu(document.querySelector('.burger'), document.querySelector('.menu-nav'));
+}
