e4032e72da0dd99d2a0bf751d19956f61c63fb9d
Dev-Vladimir <63652808+Dev-Vladimir@users.noreply.github.com>
2021-11-18 23:41:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,7 +22,56 @@ class Menu{
 	}
 }
 
+function aboutScreenAnimation(){
+	let aboutImg = document.querySelector('.about-img'),
+		sectionHeading = document.querySelector('.about .section-heading'),
+		aboutText = document.querySelector('.about-text>.text'),
+		aboutPhoto = document.querySelector('.about-text>.photo'),
+		target = document.querySelector('.about');
+	// console.log(target);
+	aboutPhoto.style.width = '0%';
+	aboutText.style.height = '0%';
+	sectionHeading.style.height = '0%';
+	aboutImg.style.width = '0%';
+	let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+    	if (entry.isIntersecting){
+	    		anime({
+				  targets: aboutImg,
+				  width: '50%',
+				  duration: 300,
+				  easing: 'easeInOutExpo'
+				});
+				anime({
+				  targets: sectionHeading,
+				  height: '100px',
+				  duration: 300,
+				  delay: 600,
+				  easing: 'easeInOutExpo'
+				});
+				// anime({
+				//   targets: aboutText,
+				//   height: 'auto',
+				//   duration: 300,
+				//   delay: 1200,
+				//   easing: 'easeInOutExpo'
+				// });
+				anime({
+				  targets: aboutPhoto,
+				  width: '50%',
+				  duration: 300,
+				  delay: 600,
+				  easing: 'easeInOutExpo'
+				});
+	    	}
+	    })
+	}, { threshold: 0.5 })
+	observer.observe(target)
+}
+
 
 window.onload = function(){
 	new Menu(document.querySelector('.burger'), document.querySelector('.menu-nav'));
+	aboutScreenAnimation();
 }
+
